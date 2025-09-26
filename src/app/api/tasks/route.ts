cf5c65046e3441f8293fb1760daab523fa3573c0
VINAY SINGAL <vinaysingal123ghn@gmail.com>
2025-09-26 17:22:55 +0530

    Update route.ts

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/db";
-import Task from "@/lib/models/Task";
+import { connectDB } from "../../../../../lib/db";
+import User from "../../../../../lib/models/User";
 
 import jwt from "jsonwebtoken";
 
